21488e8283fc3ab02403019d9ea54ebbfeb1cdd1
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:11:33 -0400

    finished sections 19 - 21

--- sections/9/course115.sql
+++ sections/9/course115.sql
@@ -1,5 +1,24 @@
 USE employees;
 
+/* HAVING syntax
+SELECT column_name(s)
+FROM table_name
+WHERE conditions
+GROUP BY column_name(s)
+HAVING conditions
+ORDER BY column_name(s);
+
+NOTES:
+* After HAVING, you can have a condition with an aggregate function. WHERE cannot use aggregate functions within its conditions.
+
+* WHERE 
+	- allows us to set conditions that refer to subsets of individual rows (applied before re-organizing the output into groups)
+* HAVING 
+	- you cannot have both an aggregated and a non-aggregated condition in the HAVING clause
+    
+Aggregate functions – GROUP BY and HAVING
+General conditions - WHERE
+*/
 SELECT emp_no, AVG(salary) AS avg_emp_salary
 FROM salaries
 GROUP BY emp_no
