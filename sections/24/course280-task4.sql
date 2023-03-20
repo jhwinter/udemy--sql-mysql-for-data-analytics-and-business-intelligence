7d68bdc7ad92398a8106b326b05b67cfedd3fb3b
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:11:34 -0400

    Completed sections 23 - 25

---
+++ sections/24/course280-task4.sql
@@ -0,0 +1,39 @@
+/*
+Create an SQL stored procedure that will allow you to obtain the 
+average male and female salary per department within a certain 
+salary range. Let this range be defined by two values the user can 
+insert when calling the procedure.
+
+Finally, visualize the obtained result-set in Tableau as a double bar 
+chart. 
+*/
+
+USE employees_mod;
+SELECT 
+	MIN(salary) AS min_salary, 
+    MAX(salary) AS max_salary 
+FROM t_salaries;
+
+
+USE employees_mod;
+DROP PROCEDURE IF EXISTS filter_salary;
+
+DELIMITER $$
+CREATE PROCEDURE filter_salary(IN p_min_salary FLOAT, IN p_max_salary FLOAT)
+BEGIN
+	SELECT
+		e.gender,
+		d.dept_name,
+		AVG(s.salary) AS avg_salary      
+    FROM t_salaries s
+    JOIN t_employees e ON s.emp_no = e.emp_no
+    JOIN t_dept_emp de ON e.emp_no = de.emp_no
+    JOIN t_departments d ON de.dept_no = d.dept_no
+	WHERE s.salary BETWEEN p_min_salary AND p_max_salary
+    GROUP BY d.dept_no, e.gender
+    ORDER BY d.dept_no;
+END$$
+DELIMITER ;
+
+
+CALL filter_salary(50000, 90000);
